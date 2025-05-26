7e345710e4c86eca7dde9feb27ce34a6b8abd552
Senge-LaFleur <asosenge@gmail.com>
2025-05-26 12:37:32 +0000

    New modification today again

--- notification/src/main/java/com/nexastay/notification/NotificationApplication.java
+++ notification/src/main/java/com/nexastay/notification/NotificationApplication.java
@@ -2,8 +2,11 @@ package com.nexastay.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.nexastay.notification.model")
 public class NotificationApplication {
 
 	public static void main(String[] args) {
